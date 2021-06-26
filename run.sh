b36da5e747cde39555645ff2ac1fcbd35ff7e3a8
Ubuntu <ubuntu@ip-172-31-26-66.ec2.internal>
2021-06-26 03:10:05 +0000

    fix feature selection

---
+++ run.sh
@@ -0,0 +1,3 @@
+python profiling-recipe/profiles/profiling_pipeline.py --config config_files/config_feature_select_all.yml
+python profiling-recipe/profiles/profiling_pipeline.py --config config_files/config_feature_select_batch.yml
+python profiling-recipe/profiles/profiling_pipeline.py --config config_files/config_feature_select_plate.yml
